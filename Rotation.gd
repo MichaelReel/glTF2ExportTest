dfeb41a88516810e030c108f345c445340291d1f
Michael Reel <mykilr@gmail.com>
2020-08-24 20:57:24 +0100

    Added playing the first animation as a loop on running the tester scene.

--- Rotation.gd
+++ Rotation.gd
@@ -2,9 +2,30 @@ extends Spatial
 
 const ROT_SPEED := 0.4
 
+var animation_player = null
+var animation_list : Dictionary = {}
+
 func _ready():
 	var test_scene = load("res://Scene_Setup.gltf").instance()
 	add_child(test_scene)
+	
+	for c in test_scene.get_children():
+		if c is AnimationPlayer:
+			animation_player = c
+	
+	if animation_player == null:
+		return
+	
+	for animation_name in animation_player.get_animation_list():
+		var animation : Animation = animation_player.get_animation(animation_name)
+		animation_list[animation_name] = animation
+		animation.set_loop(true)
+	
+	print (str(animation_list))
+	
+	if animation_list.size() > 0:
+		animation_player.play(animation_list.keys()[0])
+	
 
 func _process(delta):
 	rotation.y += delta * ROT_SPEED
